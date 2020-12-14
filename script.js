b3e8387d5eeb3c0f886061fc624d16ef49c62055
tibraj <tibraj94@gmail.com>
2020-12-14 11:05:36 -0500

    event listener for submission

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log('submit');
+})
