e7ca3647a48cca55401df9048169a95d603fd57e
tibraj <tibraj94@gmail.com>
2020-12-14 11:40:31 -0500

    character length validation for username and password

--- script.js
+++ script.js
@@ -35,7 +35,19 @@ function checkRequired(inputArray) {
     });
 }
 
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be no more than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     checkRequired([username, email, password, confirmPassword]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
 })
