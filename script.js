34085a15a041ca1d0d517da821691da4391bcce4
tibraj <tibraj94@gmail.com>
2020-12-14 11:14:58 -0500

    validation for proper email format

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if(username.value === '') {
