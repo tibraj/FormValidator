07f1fef422e36d46831386c3b696750f40d62de0
tibraj <tibraj94@gmail.com>
2020-12-14 11:10:35 -0500

    validation for username presence

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
 
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if(username.value === '') {
